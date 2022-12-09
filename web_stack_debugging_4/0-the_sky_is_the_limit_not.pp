994d848002a629eece3633949761b0d5e7097f57
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2022-12-09 08:55:05 -0800

    done

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,7 @@ service { 'nginx':
 }
 #web server setup featuring Nginx
 exec { 'change ULIMIT':
-  command  => 'sed -i "s/ULIMIT=\"-n 100\"/ULIMIT=\"-n 100\"/g" /etc/default/nginx',
+  command  => 'sed -i "s/ULIMIT=\"-n 100\"/ULIMIT=\"-n 2000\"/g" /etc/default/nginx',
   provider => 'shell',
   notify   => Service['nginx']
 }
